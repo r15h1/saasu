739b370c8054d556525852842cab6e60d3dbc163
r15h1 <tapsee.rishi@gmail.com>
2017-06-08 13:37:49 -0400

    database script with data and schema in a single DBScript.sql file

--- Scripts/01CreateDatabase.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tenants (
-    tenantid serial primary key not null,
-    name varchar(100) not null,
-    email varchar(320) not null,
-    datecreated timestamp with time zone not null,
-    isactive boolean not null
-    )
